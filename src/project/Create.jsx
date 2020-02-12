9e9742f4e31a28b0c856720dd9053e3ab71f9e21
sunyue <sy199505@live.com>
2020-02-12 11:07:37 +0800

    检测项配置修改

--- src/project/Create.jsx
+++ src/project/Create.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { 
 	Button,
+	Checkbox,
+	Icon,
 	Input,
+	Popover,
 	Radio,
-	Select, 
-	Tabs } from 'antd';
+	Select,
+	Switch,
+	Tabs
+} from 'antd';
 
 import style from './create.module.scss';
 
 const { TabPane } = Tabs;
 const { Option } = Select;
+const CheckboxGroup = Checkbox.Group;
 
 function onChange(value) {
   console.log(`selected ${value}`);
@@ -63,7 +69,7 @@ const importMethodOptions = [
 		label: "压缩包",
 		value: "archive"
 	}
-]
+];
 
 export default class Create extends React.Component {
 	static propTypes = {
@@ -97,20 +103,20 @@ export default class Create extends React.Component {
 	render() {
 		return (
 			<div className={['radius', 'card', `${style.filling}`].join(' ')}>
-			  <Tabs className={`${style.tabs}`} defaultActiveKey="1" onChange={this.callback}>
+			  <Tabs className={`${style.tabs}`} defaultActiveKey="2" onChange={this.callback}>
 			    <TabPane tab="项目信息" key="1">
-			      <div className={`${style.flexColumn}`}>
+			      <div className={[`${style.flexColumn}`, "marginB20"].join(' ')}>
 			      	<label>项目名称</label>
 			      	<Input className={`${style.input}`} />
 			      </div>
-			      <div className={`${style.flexColumn}`}>
+			      <div className={[`${style.flexColumn}`, "marginB20"].join(' ')}>
 			      	<label>系统位数</label>
 			      	<Radio.Group
 			      	  options={bitsOptions}
 			      	  onChange={this.bitsOptionChange}
 			      	  value={this.state.bitsSelect} />
 			      </div>
-			      <div className={[`${style.flexColumn}`, `${style.alignTop}`].join(' ')}>
+			      <div className={[[`${style.flexColumn}`, "marginB20"].join(' '), `${style.alignTop}`].join(' ')}>
 			      	<label>导入方式</label>
 			      	<Radio.Group
 			      		className={`${style.flexRow}`}
@@ -120,7 +126,70 @@ export default class Create extends React.Component {
 			      </div>
 			    </TabPane>
 			    <TabPane tab="检测项配置" key="2">
-			      Content of Tab Pane 2
+			      <div className={[`${style.flexColumn}`, "marginB20"].join(' ')}>
+						  <Select
+						  	className={[`${style.select}`, "marginR20"].join(' ')}
+						    showSearch
+						    style={{ width: 200 }}
+						    placeholder="Select a person"
+						    optionFilterProp="children"
+						    onChange={onChange}
+						    onSearch={onSearch}
+						    filterOption={(input, option) =>
+						      option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+						    }
+						  >
+						    <Option value="jack">Jack</Option>
+						    <Option value="lucy">Lucy</Option>
+						    <Option value="tom">Tom</Option>
+						  </Select>
+						  <Button icon="plus">新增配置</Button>
+			      </div>
+			      <div className={[`${style.flexColumn}`, "marginB20"].join(' ')}>
+			      	<span className="marginR20">代码克隆分析</span>
+			      	<Switch className="marginR20" checkedChildren="开" unCheckedChildren="关" defaultChecked />
+              <Popover content={``}>
+                <Icon className="cursorPointer marginR20" type="question-circle" />
+              </Popover>
+			      	<span className="marginR20">代码度量</span>
+			      	<Switch className="marginR20" checkedChildren="开" unCheckedChildren="关" defaultChecked />
+              <Popover content={``}>
+                <Icon className="cursorPointer marginR20" type="question-circle" />
+              </Popover>
+			      </div>
+			      <div className={[`${style.flexRow}`, `${style.checkGroup}`].join(' ')}>
+			      	<div className={[`${style.flexColumn}`, `${style.checkGroupItem}`].join(' ')}>
+			          <div className={`${style.nameWrap}`}>
+				          <Icon type="down" />
+				          <span className={`${style.name}`}>C/C++语言检测配置项</span>
+			          </div>
+		          </div>
+		          	{[1, 2, 3].map((item, i) => {
+		          		return (
+					          <div className={`${style.checkGroupItem}`}>
+					          	<Checkbox onChange={() => {}}>222</Checkbox>
+											<div className={style.flexColumn}>
+									      <Radio.Group onChange={() => {}} value={1}>
+									        <Radio value={1}>A</Radio>
+									        <Radio value={2}>B</Radio>
+									      </Radio.Group>
+									      <Checkbox.Group options={['Apple', 'Pear', 'Orange']} defaultValue={['Apple']} onChange={() => {}} />
+									      <Icon className={["cursorPointer", `${style.iconDanger}`].join(' ')} type="question-circle" />
+								      </div>
+					          </div>
+		          		)
+		          	})}
+			      </div>
+			      <div className={[`${style.flexRow}`, `${style.checkGroup}`].join(' ')}>
+			      	<div className={[`${style.flexColumn}`, `${style.nameWrap}`].join(' ')}>
+				      	<Icon type="down" />
+			          <div className={`${style.name}`}>自定义配置项</div>
+		          </div>
+		          <div className={``}>
+		          	<Checkbox onChange={() => {}}>222</Checkbox>
+					      <Icon className="cursorPointer" type="delete" />
+		          </div>
+			      </div>
 			    </TabPane>
 			    <TabPane tab="编译器配置" key="3">
 			      <div className="languageName">C/C++</div>
@@ -133,8 +202,6 @@ export default class Create extends React.Component {
 						    placeholder="Select a person"
 						    optionFilterProp="children"
 						    onChange={onChange}
-						    onFocus={onFocus}
-						    onBlur={onBlur}
 						    onSearch={onSearch}
 						    filterOption={(input, option) =>
 						      option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
